955a2716be7b64c3c54f68fa4a36fae7ff44ba49
Andrew Lampinen <lampinen@stanford.edu>
2016-11-23 18:50:32 -0800

    New description style, and grid search

--- tictactoe/ttt.py
+++ tictactoe/ttt.py
@@ -2,6 +2,13 @@ import tensorflow as tf
 import numpy
 import matplotlib.pyplot as plot
 
+####Testing parameters###############
+
+learning_rates = [0.001,0.005,0.01]
+learning_rate_decays = [0.7,0.8,0.9]
+pretraining_conditions = [True,False]
+num_runs_per = 10
+
 
 #####data parameters###########################
 n = 3 #size of board
@@ -10,21 +17,19 @@ k = 3 #number in row to win,
 #NOTE: code does not actually implement arbitrary k/n at the moment.
 
 
-#####network/learning parameters###############
 
+#####network/learning parameters###############
 nhidden = 20
 nhiddendescriptor = 20
-descriptor_output_size = (n+n+2)*(4) #(position: n rows + n columns + 2 diagonals) * (interesting state: 3 in row for me, 3 in row for him, unblocked 2 in row for me, unblocked 2 in row for him) TODO: include other useful feature descriptors, e.g. "forks"
+descriptor_output_size = (n+n+2)+(4) #(position: n rows + n columns + 2 diagonals) + (interesting state: 3 in row for me, 3 in row for him, unblocked 2 in row for me, unblocked 2 in row for him) TODO: include other useful feature descriptors, e.g. "forks"
 discount_factor = 0.98
-eta = 0.005
-eta_decay = 0.8 #Multiplicative decay per epoch
+#eta = 0.005
+#description_eta = 0.005
+#eta_decay = 0.8 #Multiplicative decay per epoch
+description_eta_decay = 0.7 #Multiplicative decay per epoch
 
 
 ###############################################
-tf.set_random_seed(1) 
-numpy.random.seed(1)
-
-
 
 def threeinrow(state): #helper, expects state to be in square shape, only looks for positive 3 in row
     return numpy.any(numpy.sum(state,axis=0) == 3) or numpy.any(numpy.sum(state,axis=1) == 3) or numpy.sum(numpy.diagonal(state)) == 3 or  numpy.sum(numpy.diagonal(numpy.fliplr(state))) == 3 
@@ -45,50 +50,47 @@ def reward(state):
 
 #(position: n rows + n columns + 2 diagonals) * (interesting state: 3 in row for me, 3 in row for him, unblocked 2 in row for me, unblocked 2 in row for him) TODO: include other useful feature descriptors, e.g. "forks"
 def description_target(state): #helper, generates description target for a given state
-    if unblockedopptwo(state) or unblockedopptwo(-state) or threeinrow(state) or threeinrow(-state):
-	target = []
-	#rows
-	for i in xrange(n):
-	    if numpy.sum(state[i,:]) == 3:
-		target.extend([1,-1,-1,-1])	
-	    elif numpy.sum(state[i,:]) == -3:
-		target.extend([-1,1,-1,-1])	
-	    elif numpy.sum(state[i,:]) == 2:
-		target.extend([-1,-1,1,-1])	
-	    elif numpy.sum(state[i,:]) == -2:
-		target.extend([-1,-1,-1,1])	
-	    else:
-		target.extend([-1,-1,-1,-1])	
-	#columns
-	for i in xrange(n):
-	    if numpy.sum(state[:,i]) == 3:
-		target.extend([1,-1,-1,-1])	
-	    elif numpy.sum(state[:,i]) == -3:
-		target.extend([-1,1,-1,-1])	
-	    elif numpy.sum(state[:,i]) == 2:
-		target.extend([-1,-1,1,-1])	
-	    elif numpy.sum(state[:,i]) == -2:
-		target.extend([-1,-1,-1,1])	
-	    else:
-		target.extend([-1,-1,-1,-1])	
-	#diagonals
-	diag1 = numpy.diag(state)
-	diag2 = numpy.diag(numpy.fliplr(state)) 
-	for d in [diag1,diag2]:
-	    if numpy.sum(d) == 3:
-		target.extend([1,-1,-1,-1])	
-	    elif numpy.sum(d) == -3:
-		target.extend([-1,1,-1,-1])	
-	    elif numpy.sum(d) == 2:
-		target.extend([-1,-1,1,-1])	
-	    elif numpy.sum(d) == -2:
-		target.extend([-1,-1,-1,1])	
-	    else:
-		target.extend([-1,-1,-1,-1])	
-	target = numpy.array(target)
-    else: #Nothing here that goes in current descriptions
-	target = -numpy.ones(descriptor_output_size)
-    return target
+    target = []
+    for i in xrange(n):
+	target.extend([1 if i == j else -1 for j in xrange(n+n+2)])
+	if numpy.sum(state[i,:]) == 3:
+	    target.extend([1,-1,-1,-1])	
+	elif numpy.sum(state[i,:]) == -3:
+	    target.extend([-1,1,-1,-1])	
+	elif numpy.sum(state[i,:]) == 2:
+	    target.extend([-1,-1,1,-1])	
+	elif numpy.sum(state[i,:]) == -2:
+	    target.extend([-1,-1,-1,1])	
+	else:
+	    target.extend([-1,-1,-1,-1])	
+    for i in xrange(n):
+	target.extend([1 if i == j-3 else -1 for j in xrange(n+n+2)])
+	if numpy.sum(state[:,i]) == 3:
+	    target.extend([1,-1,-1,-1])	
+	elif numpy.sum(state[:,i]) == -3:
+	    target.extend([-1,1,-1,-1])	
+	elif numpy.sum(state[:,i]) == 2:
+	    target.extend([-1,-1,1,-1])	
+	elif numpy.sum(state[:,i]) == -2:
+	    target.extend([-1,-1,-1,1])	
+	else:
+	    target.extend([-1,-1,-1,-1])	
+    diag = [numpy.diag(state),numpy.diag(numpy.fliplr(state))]
+    for i in xrange(2):
+	d = diag[i]
+	target.extend([1 if i == j-6 else -1 for j in xrange(n+n+2)])
+	if numpy.sum(d) == 3:
+	    target.extend([1,-1,-1,-1])	
+	elif numpy.sum(d) == -3:
+	    target.extend([-1,1,-1,-1])	
+	elif numpy.sum(d) == 2:
+	    target.extend([-1,-1,1,-1])	
+	elif numpy.sum(d) == -2:
+	    target.extend([-1,-1,-1,1])	
+	else:
+	    target.extend([-1,-1,-1,-1])	
+    target = numpy.array(target)
+    return target.reshape(8,descriptor_output_size)
 ##########Opponents##############
 
 def random_opponent(state):
@@ -126,6 +128,7 @@ def update_state(state,selection):
 initialized_stuff = {} #Dictionary to hold weights, etc., to share initilizations between network instantiations (for fair comparison)
 class Q_approx(object):
     def __init__(self):
+	global initialized_stuff
 	self.input_ph = tf.placeholder(tf.float32, shape=[n*n,1])
 	self.target_ph = tf.placeholder(tf.float32, shape=[n*n,1])
 	if initialized_stuff == {}:
@@ -164,13 +167,8 @@ class Q_approx(object):
 	self.curr_eta = eta
 	self.sess = None
 
-    def initialize_TF_sess(self):
-	self.sess = tf.Session()
-	self.sess.run(tf.initialize_all_variables())
-
-
-    def __del__(self):
-	self.sess.close()	 
+    def set_TF_sess(self,sess):
+	self.sess = sess
 
     def Q(self,state,keep_prob=1.0): #Outputs estimated Q-value for each move in this state
 	return self.sess.run(self.output,feed_dict={self.input_ph: state.reshape((9,1)),self.keep_prob: keep_prob})  
@@ -216,11 +214,13 @@ class Q_approx(object):
 
 class Q_approx_and_descriptor(Q_approx):
     def __init__(self):
+	global initialized_stuff
 	super(Q_approx_and_descriptor,self).__init__()
 	self.description_target_ph = tf.placeholder(tf.float32, shape=[descriptor_output_size,1])
+	self.description_input_ph = tf.placeholder(tf.float32, shape=[descriptor_output_size,1])
 	if ('W3d' not in initialized_stuff.keys()):
 	    
-	    self.W3d = tf.Variable(tf.random_normal([nhiddendescriptor,nhidden],0,0.1))
+	    self.W3d = tf.Variable(tf.random_normal([nhiddendescriptor,nhidden+descriptor_output_size],0,0.1))
 	    self.b3d = tf.Variable(tf.random_normal([nhiddendescriptor,1],0,0.1))
 	    self.W4d = tf.Variable(tf.random_normal([descriptor_output_size,nhiddendescriptor],0,0.1))
 	    self.b4d = tf.Variable(tf.random_normal([descriptor_output_size,1],0,0.1))
@@ -233,19 +233,47 @@ class Q_approx_and_descriptor(Q_approx):
 	    self.b3d = tf.Variable(initialized_stuff['b3d'].initialized_value())
 	    self.W4d = tf.Variable(initialized_stuff['W4d'].initialized_value())
 	    self.b4d = tf.Variable(initialized_stuff['b4d'].initialized_value())
-	self.description_output = tf.nn.tanh(tf.matmul(self.W4d,tf.nn.tanh(tf.matmul(self.W3d,tf.nn.dropout(tf.nn.tanh(tf.matmul(self.W2,tf.nn.dropout(tf.nn.tanh(tf.matmul(self.W1,self.input_ph)+self.b1),keep_prob=self.keep_prob))+self.b2),keep_prob=self.keep_prob))+self.b3d))+self.b4d)
+	self.description_output = tf.nn.tanh(tf.matmul(self.W4d,tf.nn.tanh(tf.matmul(self.W3d,tf.concat(0,(tf.nn.dropout(tf.nn.tanh(tf.matmul(self.W2,tf.nn.dropout(tf.nn.tanh(tf.matmul(self.W1,self.input_ph)+self.b1),keep_prob=self.keep_prob))+self.b2),keep_prob=self.keep_prob),self.description_input_ph)))+self.b3d))+self.b4d)
 	self.description_error = tf.square(self.description_output-self.description_target_ph)
 	self.description_train = self.optimizer.minimize(tf.reduce_sum(self.description_error))
+	self.curr_description_eta = description_eta
 
-    def describe(self,state,keep_prob=1.0): #Outputs estimated description for the current state
-	return self.sess.run(self.description_output,feed_dict={self.input_ph: state.reshape((9,1)),self.keep_prob: keep_prob})  
+    def describe(self,state,keep_prob=1.0): #Outputs estimated descriptions for the current state
+	this_description = []
+	for j in xrange(n+n+2):
+	    this_description_input = numpy.roll([1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],j).reshape((descriptor_output_size,1))
+	    this_description.append(self.sess.run(self.description_output,feed_dict={self.input_ph: state.reshape((9,1)),self.description_input_ph: this_description_input,self.keep_prob: keep_prob})) 
+	return 
 
 
-    def get_description_error(self,state,keep_prob=1.0): #Outputs description cross entropy for the current state
-	return self.sess.run(self.description_error,feed_dict={self.input_ph: state.reshape((9,1)),self.description_target_ph: description_target(state).reshape((descriptor_output_size,1)),self.keep_prob: keep_prob})  
+    def get_description_error(self,state,keep_prob=1.0): #Outputs description sum across possible positions of squared error 
+	state_full_description_target = description_target(state)
+	SSE = numpy.zeros((descriptor_output_size,1))
+	for j in xrange(n+n+2):
+	    this_description_target = state_full_description_target[j].reshape((descriptor_output_size,1))
+	    this_description_input = numpy.roll([1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],j).reshape((descriptor_output_size,1))
+	    SSE += self.sess.run(self.description_error,feed_dict={self.input_ph: state.reshape((9,1)),self.description_input_ph: this_description_input,self.description_target_ph: this_description_target,self.keep_prob: keep_prob}) 
+	return SSE 
 
-    def train_description(self,state):
-	self.sess.run(self.description_train,feed_dict={self.input_ph: state.reshape((9,1)),self.description_target_ph: description_target(state).reshape((descriptor_output_size,1)),self.keep_prob: 0.5,self.eta: self.curr_eta}) 
+    def train_description(self,state): #right now, only trains on describing events given locations TODO: also include picking locations for events, but need to handle multiple occurrences
+	state_full_description_target = description_target(state)
+	boring_list = [] #list of places where nothing is happening, a few will be sampled at the end for balance 
+	for i in xrange(n+n+2):
+	    this_description_target = state_full_description_target[i].reshape((descriptor_output_size,1))
+	    if sum(this_description_target[-4:]) > -4:
+		this_description_input = numpy.roll([1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],i).reshape((n+n+2+4,1))
+		self.sess.run(self.description_train,feed_dict={self.input_ph: state.reshape((9,1)),self.description_input_ph: this_description_input,self.description_target_ph: this_description_target,self.keep_prob: 0.5,self.eta: self.curr_description_eta}) 
+	    else:
+		boring_list.append(i)
+	    
+	#balance out with training on non-events
+	boring_list = numpy.random.permutation(boring_list)[:n+n+2-len(boring_list)]
+	for j in boring_list:
+	    this_description_target = state_full_description_target[j].reshape((descriptor_output_size,1))
+	    this_description_input = numpy.roll([1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],j).reshape((n+n+2+4,1))
+	    self.sess.run(self.description_train,feed_dict={self.input_ph: state.reshape((9,1)),self.description_input_ph: this_description_input,self.description_target_ph: this_description_target,self.keep_prob: 0.5,self.eta: self.curr_description_eta}) 
+	    
+	    
 
 #####Data generation#####################
 
@@ -324,69 +352,94 @@ def train_on_games_with_descriptions(Q_net,opponent,numgames=1000,numdescription
 	score += play_game(Q_net,opponent,train=True,description_train=((game%description_step)==0))
     return  (float(score)/numgames) 
 
-
-#network initialization
-basic_Q_net = Q_approx()
-basic_Q_net.initialize_TF_sess()
-descr_Q_net = Q_approx_and_descriptor()
-descr_Q_net.initialize_TF_sess()
-
-
-#description data creation
-descr_train_data = generate(make_ttt_array,lambda x: unblockedopptwo(x) or unblockedopptwo(-x) or threeinrow(x) or threeinrow(-x),3000)
-descr_test_data = generate(make_ttt_array,lambda x: unblockedopptwo(x) or unblockedopptwo(-x) or threeinrow(x) or threeinrow(-x),3000)
-
-
+for pretraining_condition in pretraining_conditions:
+    for eta in learning_rates:
+	description_eta = eta
+	for eta_decay in learning_rate_decays
+	    avg_descr_score_track = numpy.zeros(21)
+	    avg_descr_descr_MSE_track = numpy.zeros(21)
+	    avg_basic_score_track = numpy.zeros(21)
+	    for run in xrange(num_runs_per):
+		print "pretrain-%s_eta-%f_eta_decay-%f_run-%i" %(str(pretraining_condition),eta,eta_decay,run)
+		tf.set_random_seed(run) 
+		numpy.random.seed(run)
 
 
-#print "Description initial test (descr_Q_net):"
-#test_descriptions(descr_Q_net,descr_test_data)
-#print "Initial test (basic_Q_net):"
-#test_on_games(basic_Q_net,random_opponent,numgames=1000)
-#print "Initial test (descr_Q_net):"
-#test_on_games(descr_Q_net,random_opponent,numgames=1000)
+		initialized_stuff = {} #Dictionary to hold weights, etc., to share initilizations between network instantiations (for fair comparison)
 
 
-descr_score_track = []
-descr_descr_MSE_track = []
-basic_score_track = []
-
-#Description pretraining
-print "Pre-training descriptions..."
-train_descriptions(descr_Q_net,descr_train_data,epochs=20)
-print "Description test after pre-training (descr_Q_net):"
-test_descriptions(descr_Q_net,descr_test_data)
-
-print descr_test_data[1]
-print descr_Q_net.describe(descr_test_data[1]).reshape((8,4))
-print description_target( descr_test_data[1]).reshape((8,4))
-
-print "Training..."
-for i in xrange(50):
-    print "training epoch %i" %i
-    train_on_games(basic_Q_net,random_opponent,numgames=1000)
-    train_on_games_with_descriptions(descr_Q_net,random_opponent,numgames=1000,numdescriptions=20)
-
-    temp = test_on_games(basic_Q_net,random_opponent,numgames=1000)
-    print "basic_Q_net average score: %f" %temp
-    basic_score_track.append(temp)
-    temp = test_on_games(descr_Q_net,random_opponent,numgames=1000)
-    print "descr_Q_net average score: %f" %temp
-    descr_score_track.append(temp)
-    temp = test_descriptions(descr_Q_net,descr_test_data)
-    descr_descr_MSE_track.append(temp)
-    
-
-    basic_Q_net.curr_eta *= eta_decay
-    descr_Q_net.curr_eta *= eta_decay
+		#network initialization
+		basic_Q_net = Q_approx()
+		descr_Q_net = Q_approx_and_descriptor()
+		sess = tf.Session()
+		basic_Q_net.set_TF_sess(sess)
+		descr_Q_net.set_TF_sess(sess)
+		sess.run(tf.initialize_all_variables())
 
 
-print "Training done, final test:"
-test_on_games(basic_Q_net,random_opponent,numgames=10000)
-test_on_games(descr_Q_net,random_opponent,numgames=10000)
+		if pretraining_condition:
+		    #description data creation
+		    descr_train_data = generate(make_ttt_array,lambda x: unblockedopptwo(x) or unblockedopptwo(-x) or threeinrow(x) or threeinrow(-x),2000)
+		    descr_test_data = generate(make_ttt_array,lambda x: unblockedopptwo(x) or unblockedopptwo(-x) or threeinrow(x) or threeinrow(-x),2000)
 
-numpy.savetxt('descr_score_track.csv',descr_score_track,delimiter=',')
-numpy.savetxt('basic_score_track.csv',basic_score_track,delimiter=',')
-numpy.savetxt('descr_descr_MSE_track.csv',descr_descr_MSE_track,delimiter=',')
 
+
+		descr_score_track = []
+		descr_descr_MSE_track = []
+		basic_score_track = []
+
+		#print "Description initial test (descr_Q_net):"
+		#temp = test_descriptions(descr_Q_net,descr_test_data)
+		#print temp
+		#descr_descr_MSE_track.append(temp)
+		#print "Initial test (basic_Q_net):"
+		#temp = test_on_games(basic_Q_net,random_opponent,numgames=1000)
+		#print temp
+		#basic_score_track.append(temp)
+		#print "Initial test (descr_Q_net):"
+		#temp = test_on_games(descr_Q_net,random_opponent,numgames=1000)
+		#print temp
+		#descr_score_track.append(temp)
+
+
+		if pretraining_condition:
+		    ##Description pretraining
+#		    print "Pre-training descriptions..."
+		    train_descriptions(descr_Q_net,descr_train_data,epochs=1)
+#		    print "Description test after pre-training (descr_Q_net):"
+#		    test_descriptions(descr_Q_net,descr_test_data)
+
+		print "Training..."
+		for i in xrange(20):
+		    print "training epoch %i" %i
+		    train_on_games(basic_Q_net,random_opponent,numgames=500)
+		    train_on_games_with_descriptions(descr_Q_net,random_opponent,numgames=500,numdescriptions=50)
+
+		    temp = test_on_games(basic_Q_net,random_opponent,numgames=1000)
+		    print "basic_Q_net average score: %f" %temp
+		    basic_score_track.append(temp)
+		    temp = test_on_games(descr_Q_net,random_opponent,numgames=1000)
+		    print "descr_Q_net average score: %f" %temp
+		    descr_score_track.append(temp)
+		    temp = test_descriptions(descr_Q_net,descr_test_data)
+		    descr_descr_MSE_track.append(temp)
+		    
+
+		    if (i%2) == 1:
+			basic_Q_net.curr_eta *= eta_decay
+			descr_Q_net.curr_eta *= eta_decay
+			descr_Q_net.curr_description_eta *= description_eta_decay
+
+		sess.close()
+		tf.reset_default_graph()
+
+		avg_basic_score_track += numpy.array(basic_score_track)
+		avg_descr_score_track += numpy.array(descr_score_track)
+		avg_descr_descr_MSE_track += numpy.array(descr_descr_MSE_track)
+
+
+		
+	    numpy.savetxt('avg_descr_score_track_pretrain-%s_eta-%f_eta_decay-%f.csv'%(str(pretraining_condition),eta,eta_decay),descr_score_track,delimiter=',')
+	    numpy.savetxt('avg_basic_score_track-%s_eta-%f_eta_decay-%f.csv'%(str(pretraining_condition),eta,eta_decay),basic_score_track,delimiter=',')
+	    numpy.savetxt('avg_descr_descr_MSE_track-%s_eta-%f_eta_decay-%f.csv'%(str(pretraining_condition),eta,eta_decay),descr_descr_MSE_track,delimiter=',')
 
